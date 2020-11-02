a455562581a2da61114508e215783814b83f4553
nikita-sharov <nikita.sharov@235u.net>
2020-11-02 13:14:57 +0100

    Remove dead code

--- src/WordCounter.WinForms/OrderedWordCounting.cs
+++ src/WordCounter.WinForms/OrderedWordCounting.cs
@@ -4,20 +4,5 @@ namespace WordCounter.WinForms
 {
     public sealed class OrderedWordCounting : Dictionary<int, KeyValuePair<string, int>>
     {
-        ////public static OrderedWordCounting OrderByWordCountDescending(WordCounting counting)
-        ////{
-        ////    var orderedCounting = new OrderedWordCounting();
-        ////    IOrderedEnumerable<KeyValuePair<string, int>> orderedWordCounts =
-        ////        counting.OrderByDescending(wordCount => wordCount.Value);
-
-        ////    int index = 0;
-        ////    foreach (var wordCount in orderedWordCounts)
-        ////    {
-        ////        orderedCounting.Add(index, wordCount);
-        ////        index += 1;
-        ////    }
-
-        ////    return orderedCounting;
-        ////}
     }
 }
